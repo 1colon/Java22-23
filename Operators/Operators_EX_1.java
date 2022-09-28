94349a10e26453646b28787f052eb2a48d737f3d
1colon <1colonalmiva@hdsb.ca>
2022-09-28 01:56:56 -0400

    Initial commit

---
+++ Operators/Operators_EX_1.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Operators_EX_1 {
+
+    public static void main(String[] args) {
+        
+        // || or
+        // && and
+        // ! not
+
+
+        int a=3;
+        int b=4;
+        boolean isSunny = true;
+        if (a < b && isSunny){
+
+            System.out.println("its gonna be a good day!");
+
+        }
+
+
+    }
+}
